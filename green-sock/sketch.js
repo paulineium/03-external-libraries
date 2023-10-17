e63d752daf847bce6ae9c76e3b9db31c0776bdea
paulineium <pk2196@nyu.edu>
2023-10-16 23:01:39 -0400

    different version (uses a mouse click)

--- green-sock/sketch.js
+++ green-sock/sketch.js
@@ -1,8 +1,23 @@
-// Animation with GSAP
-gsap.to("#animated-box", {
+const elements = document.querySelectorAll(".square, .circle, .triangle");
+
+gsap.set(elements, { transformOrigin: "center" });
+
+let animation = gsap.timeline({ repeat: -1, paused: true });
+
+animation.to(elements, {
     duration: 2,
-    x: 200, // Move 200 pixels to the right
-    backgroundColor: "#e74c3c", // Change background color
-    borderRadius: "50%", // Make it a circle
-    ease: "bounce.out", // Use a bounce easing function
+    x: 200,
+    y: 200,
+    ease: "power2.inOut",
+    rotation: 360,
+    opacity: 0,
+    stagger: 0.2,
+});
+
+document.body.addEventListener("click", () => {
+    if (animation.paused()) {
+        animation.play();
+    } else {
+        animation.restart();
+    }
 });
